23b795fc542acc4c64eb40bc8c4cf2eb7dddeca6
thomas-clr-dev <clarisse.thomas0902@gmail.com>
2025-10-24 11:18:17 +0200

    Initial Commit

---
+++ Editor/Temp.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Vhrtz.EditorOnly
+{
+    public class Temp : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
